2966ed65526da5d4a6f70730f255351e60cf44b6
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-09 07:04:24 +0100

    inventor travis-core, use a released gem

--- vendor/gems/travis-core/lib/travis/model/artifact/log.rb
+++
@@ -1,9 +0,0 @@
-class Artifact::Log < Artifact
-  class << self
-    # use job_id to avoid loading a log artifact into memory
-    def append(job_id, chars)
-      update_all(["content = COALESCE(content, '') || ?", chars], ["job_id = ?", job_id])
-    end
-  end
-end
-
