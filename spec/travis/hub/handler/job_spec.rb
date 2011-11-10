fbf793d4e61700c39b69fb0b3b075c57e03878f5
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-10 14:16:52 +0100

    add handler/job_spec

---
+++ spec/travis/hub/handler/job_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Travis::Hub::Handler::Job do
+  let(:handler) { Travis::Hub::Handler::Job.new(:event, Hashr.new(payload)) }
+  let(:job)     { stub('job', :update_attributes => nil) }
+  let(:payload) { Hashr.new }
+
+  before :each do
+    handler.stubs(:job).returns(job)
+  end
+
+  describe '#handle' do
+    it 'updates job attributes on job:test:started' do
+      job.expects(:update_attributes).with(payload)
+      handler.event = :'job:test:started'
+      handler.handle
+    end
+
+    it 'appends the log on job:test:log' do
+      ::Job::Test.expects(:append_log!)
+      handler.event = :'job:test:log'
+      handler.handle
+    end
+  end
+end
+
