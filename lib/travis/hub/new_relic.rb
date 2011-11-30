49a92c287f8073f620efada33c415ba39ef61e60
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-11-30 01:16:12 +0100

    rework the instrumentation a little

--- lib/travis/hub/new_relic.rb
+++ lib/travis/hub/new_relic.rb
@@ -1,31 +1,33 @@
 module Travis
   class Hub
     module NewRelic
-      def receive(message, payload)
-        params = {
-          :message => message,
-          :payload => payload
-        }
-        perform_action_with_newrelic_trace(:category => :controller, :name => :receive, :params => params) do
-          super
+      module HubInstrumentation
+        def receive(message, payload)
+          params = {
+            :message => message,
+            :payload => payload
+          }
+          perform_action_with_newrelic_trace(:category => :controller, :name => :receive, :params => params) do
+            super
+          end
         end
       end
-    end
 
-    def self.monitor
-      puts "Starting New Relic with env:#{ENV['ENV']}"
-      require 'newrelic_rpm'
+      def self.monitor
+        puts "Starting New Relic with env:#{ENV['ENV']}"
+        require 'newrelic_rpm'
 
-      Travis::Hub.class_eval do
-        include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-        include Travis::Hub::NewRelic
+        Travis::Hub.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          include Travis::Hub::NewRelic::HubInstrumentation
+        end
+
+        NewRelic::Agent.manual_start(:env => ENV['ENV'])
+      rescue Exception => e
+        puts 'New Relic Agent refused to start!'
+        puts e.message
+        puts e.backtrace
       end
-
-      NewRelic::Agent.manual_start(:env => ENV['ENV'])
-    rescue Exception => e
-      puts 'New Relic Agent refused to start!'
-      puts e.message
-      puts e.backtrace
     end
   end
 end
