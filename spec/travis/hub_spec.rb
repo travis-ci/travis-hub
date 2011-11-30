df8d086369167d44d582182771f769e0309de0ab
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-30 02:57:22 +0100

    include the implementation as an anonymous module in Travis::Hub, so additional modules can be included more easily on top of it

--- spec/travis/hub_spec.rb
+++ spec/travis/hub_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe Travis::Hub do
   let(:hub)     { Travis::Hub.new }
-  let(:payload) { hub.send(:decode, '{ "id": 1 }') }
 
   describe 'decode' do
     it 'decodes a json payload' do
-      payload['id'].should == 1
+      hub.send(:decode, '{ "id": 1 }')['id'].should == 1
     end
   end
 end
