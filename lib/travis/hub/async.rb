735e716f12346774e9c69296b064eafafe7b907a
Sven Fuchs <svenfuchs@artweb-design.de>
2011-12-09 11:19:51 +0100

    try not using async at all for now

--- lib/travis/hub/async.rb
+++ lib/travis/hub/async.rb
@@ -12,9 +12,9 @@ Async.class_eval do
   end
 end
 
-unless ENV['ENV'] == 'test'
-  %w(Archive Email Irc Pusher Webhook).each do |name|
-    handler = Travis::Notifications::Handler.const_get(name)
-    handler.async :notify
-  end
-end
+# unless ENV['ENV'] == 'test'
+#   %w(Archive Email Irc Pusher Webhook).each do |name|
+#     handler = Travis::Notifications::Handler.const_get(name)
+#     handler.async :notify
+#   end
+# end
