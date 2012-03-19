24775744b8b1affc8d5467a27fa1d8bea5c8d072
Sven Fuchs <svenfuchs@artweb-design.de>
2012-03-19 12:08:42 +0100

    prefer ENV over RAILS_ENV in thor travis:hub:start (so we actually use the staging conf on staging)

--- lib/travis/hub/cli.rb
+++ lib/travis/hub/cli.rb
@@ -9,7 +9,7 @@ module Travis
       namespace 'travis:hub'
 
       desc 'start', 'Consume AMQP messages from the worker'
-      method_option :env, :aliases => '-e', :default => ENV['RAILS_ENV'] || ENV['ENV'] || 'development'
+      method_option :env, :aliases => '-e', :default => ENV['ENV'] || ENV['RAILS_ENV'] || 'development'
       def start
         ENV['ENV'] = options['env']
         preload_constants!
