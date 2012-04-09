2fafcc6a83970a07c2a08c3bcb316948c78962f3
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-04-09 02:48:46 +0200

    reduce the support payloads to what is actually needed

--- spec/support/payloads.rb
+++ spec/support/payloads.rb
@@ -1,57 +1,28 @@
 GITHUB_PAYLOADS = {
-  "gem-release" => {
-    :repository => {
-      :url => "http://github.com/svenfuchs/gem-release",
-      :name => "gem-release",
-      :owner => {
-        :email => "svenfuchs@artweb-design.de",
-        :name => "svenfuchs"
+  "gem-release" => %({
+    "repository": {
+      "url": "http://github.com/svenfuchs/gem-release",
+      "name": "gem-release",
+      "description": "Release your gems with ease",
+      "owner": {
+        "email": "svenfuchs@artweb-design.de",
+        "name": "svenfuchs"
       }
     },
-    :commits => [{
-      :id => "9854592",
-      :message => "Bump to 0.0.15",
-      :timestamp => "2010-10-27 04:32:37",
-      :committer => {
-        :name => "Sven Fuchs",
-        :email => "svenfuchs@artweb-design.de"
+    "commits": [{
+      "id":        "9854592",
+      "message":   "Bump to 0.0.15",
+      "timestamp": "2010-10-27 04:32:37",
+      "committer": {
+        "name":  "Sven Fuchs",
+        "email": "svenfuchs@artweb-design.de"
       },
-      :author => {
-        :name => "Christopher Floess",
-        :email => "chris@flooose.de"
+      "author": {
+        "name":  "Christopher Floess",
+        "email": "chris@flooose.de"
       }
     }],
-    :ref => "refs/heads/master",
-    :compare => "https://github.com/svenfuchs/gem-release/compare/af674bd...9854592"
-  }
+    "ref": "refs/heads/master",
+    "compare": "https://github.com/svenfuchs/gem-release/compare/af674bd...9854592"
+  })
 }
-
-WORKER_PAYLOADS = {
-  'job:configure:started'  => { 'id' => 1, 'state' => 'started',  'started_at'  => '2011-01-01 00:00:00 +0200' },
-  'job:configure:finished' => { 'id' => 1, 'state' => 'finished', 'finished_at' => '2011-01-01 00:01:00 +0200', 'config' => { 'rvm' => ['1.8.7', '1.9.2'] } },
-  'job:test:started'       => { 'id' => 1, 'state' => 'started',  'started_at'  => '2011-01-01 00:02:00 +0200' },
-  'job:test:log'           => { 'id' => 1, 'log' => '... appended' },
-  'job:test:log:1'         => { 'id' => 1, 'log' => 'the '  },
-  'job:test:log:2'         => { 'id' => 1, 'log' => 'full ' },
-  'job:test:log:3'         => { 'id' => 1, 'log' => 'log'   },
-  'job:test:finished'      => { 'id' => 1, 'state' => 'finished', 'finished_at' => '2011-01-01 00:03:00 +0200', 'status' => 0, 'log' => 'the full log' }
-}
-
-QUEUE_PAYLOADS = {
-  'job:configure' => {
-    :build      => { :id => 1, :commit => '9854592', :branch => 'master' },
-    :repository => { :id => 1, :slug => 'svenfuchs/gem-release' },
-    :queue      => 'builds'
-  },
-  'job:test:1' => {
-    :build      => { :id => 2, :number => '1.1', :commit => '9854592', :branch => 'master', :config => { :rvm => '1.8.7' } },
-    :repository => { :id => 1, :slug => 'svenfuchs/gem-release' },
-    :queue      => 'builds'
-  },
-  'job:test:2' => {
-    :build      => { :id => 3, :number => '1.2', :commit => '9854592', :branch => 'master', :config => { :rvm => '1.9.2' } },
-    :repository => { :id => 1, :slug => 'svenfuchs/gem-release' },
-    :queue      => 'builds'
-  }
-}
-
