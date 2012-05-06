f78f4d448ad653392c90ed5552e5778f1441c4fe
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-06 12:43:17 +0200

    rename :build key to :job for the job:configure payload

--- lib/travis/hub/handler.rb
+++ lib/travis/hub/handler.rb
@@ -13,14 +13,14 @@ module Travis
       class << self
         def for(event, payload)
           case event.to_s
-          when /^job/
-            Handler::Job.new(event, payload)
-          when /^worker/
-            Handler::Worker.new(event, payload)
           when /^request/
             Handler::Request.new(event, payload)
           when /^configure/
             Handler::Configure.new(event, payload)
+          when /^job/
+            Handler::Job.new(event, payload)
+          when /^worker/
+            Handler::Worker.new(event, payload)
           else
             raise "Unknown message type: #{event.inspect}"
           end
--- lib/travis/hub/handler/configure.rb
+++ lib/travis/hub/handler/configure.rb
@@ -4,9 +4,14 @@ module Travis
   class Hub
     class Handler
       class Configure < Handler
+        def initialize(event, payload)
+          super
+          self.payload.job = payload.build unless payload.job?
+        end
+
         def handle
           track_event(:received)
-          configure_build
+          configure(result)
           track_event(:completed)
         rescue StandardError => e
           track_event(:failed)
@@ -15,30 +20,19 @@ module Travis
 
         protected
 
-          def configure_build
-            debug "Retrieving .travis.yml for payload #{payload.inspect}"
-            result = ::Travis::Tasks::ConfigureBuild.new(commit_details).run
-            update_job(result)
-          end
-
-          def update_job(result)
-            debug "Updating Job (id:#{job_id}) with #{result.inspect}"
-            job = ::Job.find(job_id)
+          def configure(result)
+            debug "Updating Job (id:#{payload.job.id}) with #{result.inspect}"
+            job = ::Job.find(payload.job.id)
             job.update_attributes(result)
           end
 
-          def commit_details
-            payload[:build]
-          end
-
-          def job_id
-            payload[:build][:id]
+          def result
+            debug "Retrieving .travis.yml for payload #{payload.inspect}"
+            Task::Request::Configure.new(payload.job).run
           end
 
           def track_event(name)
-            meter_name = 'travis.hub.configure'
-            meter_name = "#{meter_name}.#{name.to_s}"
-            Metriks.meter(meter_name).mark
+            Metriks.meter("travis.hub.configure.#{name}").mark
           end
       end
     end
