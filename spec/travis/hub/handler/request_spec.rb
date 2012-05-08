44a84321a7eb3a272bf97e19957bf2f7f8aee288
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-08 23:57:56 +0200

    cleanup

--- spec/travis/hub/handler/request_spec.rb
+++ spec/travis/hub/handler/request_spec.rb
@@ -1,69 +1,74 @@
 require 'spec_helper'
 
+RSpec::Matchers.define :meter do |meter|
+  match do |action|
+    expect(&action).to change { Metriks.meter(meter).count }
+  end
+end
+
 describe Travis::Hub::Handler::Request do
+  let(:payload) { { :type => 'push', :credentials => { :login => "user", :token => "12345" }, :request => GITHUB_PAYLOADS['gem-release'] } }
   let(:handler) { Travis::Hub::Handler::Request.new(:request, Hashr.new(payload)) }
   let(:user)    { stub('user', :login => 'user') }
-  let(:token)   { stub('token', :user => user) }
-  let(:payload) { { :type => 'push', :credentials => { :login => "user", :token => "12345" }, :request => GITHUB_PAYLOADS['gem-release'] } }
 
-  describe '#handle' do
-    describe 'authorized' do
+  subject { proc { handler.handle } }
+
+  describe 'handle' do
+    describe 'given the request can be authorized' do
       before do
-        Request.stubs(:create_from).with('push', GITHUB_PAYLOADS['gem-release'], '12345').returns(true)
         User.expects(:authenticate_by_token).with('user', '12345').returns(user)
+        Request.stubs(:create_from).with('push', GITHUB_PAYLOADS['gem-release'], '12345').returns(true)
       end
 
       it "creates a valid request" do
-        handler.handle
+        subject.call
       end
 
       it "increments a counter when a request build message is received" do
-        expect { handler.handle }.to change { Metriks.meter('travis.hub.build_requests.push.received').count }
+        subject.should meter('travis.hub.build_requests.push.received')
       end
 
       it "increments a counter when a request build message is authenticated" do
-        expect { handler.handle }.to change { Metriks.meter('travis.hub.build_requests.push.received.authenticated').count }
+        subject.should meter('travis.hub.build_requests.push.received.authenticated')
       end
 
       it "increments a counter when a request build message is created" do
-        expect { handler.handle }.to change { Metriks.meter('travis.hub.build_requests.push.received.created').count }
+        subject.should meter('travis.hub.build_requests.push.received.created')
       end
 
       it "increments a counter when a request build message raises an exception" do
         Request.stubs(:create_from).raises(StandardError)
-        expect { handler.handle rescue nil }.to change { Metriks.meter('travis.hub.build_requests.push.received.failed').count }
+        proc { handler.handle rescue nil }.should meter('travis.hub.build_requests.push.received.failed')
       end
 
       it "logs an info message" do
         handler.expects(:info)
-        handler.handle
+        subject.call
       end
     end
 
-    describe 'not authorized' do
-      let(:user)  { stub('user_two', :login => 'user2') }
-      let(:token) { stub('token', :user => user) }
-
+    describe 'given the request can not be authorized' do
       before do
         User.expects(:authenticate_by_token).with('user', '12345').returns(nil)
+        Request.stubs(:create_from).never
       end
 
       it "rejects the request" do
         Request.expects(:create_from).never
-        handler.handle
+        subject.call
       end
 
       it "increments a counter when a request build message is received" do
-        expect { handler.handle }.to change { Metriks.meter('travis.hub.build_requests.push.received').count }
+        subject.should meter('travis.hub.build_requests.push.received')
       end
 
       it "does not increment a counter when a request build message is not authenticated" do
-        expect { handler.handle }.not_to change { Metriks.meter('travis.hub.build_requests.push.authenticated').count }
+        subject.should_not meter('travis.hub.build_requests.push.authenticated')
       end
 
       it "logs a warning" do
         handler.expects(:warn)
-        handler.handle
+        subject.call
       end
     end
   end
