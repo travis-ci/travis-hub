b917428d9a6af0fa26f5790fa2210b273e39689b
Sven Fuchs <svenfuchs@artweb-design.de>
2012-07-29 16:04:00 +0200

    fix the worker handler spec

--- spec/travis/hub/handler/worker_spec.rb
+++ spec/travis/hub/handler/worker_spec.rb
@@ -1,18 +1,13 @@
 require 'spec_helper'
 
 describe Travis::Hub::Handler::Worker do
-  let(:worker)  { stub('worker', :update_attributes! => nil) }
   let(:handler) { Travis::Hub::Handler::Worker.new('worker:status', payload) }
-
-  before :each do
-    handler.stubs(:worker_by).returns(worker)
-  end
 
   describe 'handle (old api, hash payload)' do
     let(:payload) { { :name => 'travis-test-1', :host => 'host', :state => 'ready' } }
 
-    it 'updates the worker states and last_seen_at attributes' do
-      worker.expects(:ping).with(payload)
+    it 'updates the worker status' do
+      Worker::Status.expects(:update).with([payload])
       handler.handle
     end
   end
@@ -21,7 +16,7 @@ describe Travis::Hub::Handler::Worker do
     let(:payload) { [{ :name => 'travis-test-1', :host => 'host', :state => 'ready' }] }
 
     it 'updates the worker states and last_seen_at attributes (array payload)' do
-      worker.expects(:ping).with(payload.first)
+      Worker::Status.expects(:update).with(payload)
       handler.handle
     end
   end
@@ -30,24 +25,10 @@ describe Travis::Hub::Handler::Worker do
     let(:payload) { { 'workers' => [{ :name => 'travis-test-1', :host => 'host', :state => 'ready' }] } }
 
     it 'updates the worker states and last_seen_at attributes' do
-      worker.expects(:ping).with(payload['workers'].first)
+      Worker::Status.expects(:update).with(payload['workers'])
       handler.handle
     end
   end
-
-  # TODO make the db available here
-  #
-  # describe 'workers' do
-  #   it 'returns workers grouped by their full_name' do
-  #     worker = Worker.create(:name => 'worker-1', :host => 'host')
-  #     handler.send(:workers)['host:worker-1'].should == worker
-  #   end
-  # end
-  #
-  # describe 'worker' do
-  #   it 'returns an existing worker instance'
-  #   it 'returns a new worker instance'
-  # end
 end
 
 
