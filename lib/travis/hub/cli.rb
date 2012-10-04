fb097c180247590ffa3acdca6586696cf53f3f5d
Sven Fuchs <me@svenfuchs.com>
2012-10-04 19:08:26 +0200

    try removing the constant preloading since we're on 1.7.0 now

--- lib/travis/hub/cli.rb
+++ lib/travis/hub/cli.rb
@@ -11,7 +11,7 @@ module Travis
       desc 'start', 'Consume AMQP messages from the worker'
       def start
         ENV['ENV'] || 'development'
-        preload_constants!
+        # preload_constants!
         Travis::Hub.start
       end
 
@@ -22,7 +22,7 @@ module Travis
           require 'travis'
 
           [Travis::Hub, Travis].each do |target|
-            target.load_constants!(:skip => [/::AssociationCollection$/])
+            target.load_constants!(:skip => [/::AssociationCollection$/], :debug => true)
           end
         end
     end
