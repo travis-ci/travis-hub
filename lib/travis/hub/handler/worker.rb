36ef964dcc17011b29619357b55a3ad1430a68c8
Sven Fuchs <me@svenfuchs.com>
2012-11-17 23:11:13 +0100

    fix worker name

--- lib/travis/hub/handler/worker.rb
+++ lib/travis/hub/handler/worker.rb
@@ -7,7 +7,7 @@ module Travis
           # TODO hot compat, remove the next line once all workers send the new payload
           reports = payload.is_a?(Hash) ? payload['workers'] || payload : payload
           reports = [reports] if reports.is_a?(Hash)
-          Travis.run_service(:update_worker, reports: reports)
+          Travis.run_service(:update_workers, reports: reports)
         end
         instrument :handle
         new_relic :handle
