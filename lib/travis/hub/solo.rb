d1d96603adaaf0879e0f661ccc6433eeeff2d95a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-03-05 00:50:47 +0100

    more logging for setup

--- lib/travis/hub/solo.rb
+++ lib/travis/hub/solo.rb
@@ -9,17 +9,28 @@ module Travis
         Travis::Async.enabled = true
         Travis::Amqp.config = Travis.config.amqp
 
+        Travis.logger.info('[hub] connecting to database')
         Travis::Database.connect
+
         if Travis.config.logs_database
+          Travis.logger.info('[hub] connecting to logs database')
           Log.establish_connection 'logs_database'
           Log::Part.establish_connection 'logs_database'
         end
 
+        Travis.logger.info('[hub] setting up sidekiq')
         Travis::Async::Sidekiq.setup(Travis.config.redis.url, Travis.config.sidekiq)
 
+        Travis.logger.info('[hub] starting exceptions reporter')
         Travis::Exceptions::Reporter.start
+
+        Travis.logger.info('[hub] setting up metrics')
         Travis::Metrics.setup
+
+        Travis.logger.info('[hub] setting up notifications')
         Travis::Notification.setup
+
+        Travis.logger.info('[hub] setting up addons')
         Travis::Addons.register
 
         declare_exchanges_and_queues
@@ -72,6 +83,7 @@ module Travis
         end
 
         def declare_exchanges_and_queues
+          Travis.logger.info('[hub] connecting to amqp')
           channel = Travis::Amqp.connection.create_channel
           channel.exchange 'reporting', durable: true, auto_delete: false, type: :topic
           channel.queue 'builds.linux', durable: true, exclusive: false
