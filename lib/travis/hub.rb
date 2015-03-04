1199ac767f086ff4e7d1d30d546831ec3e5f9868
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-03-05 00:50:30 +0100

    less logging for requires

--- lib/travis/hub.rb
+++ lib/travis/hub.rb
@@ -2,35 +2,18 @@ require 'multi_json'
 
 require 'travis'
 
-Travis.logger.info('[hub] loading travis/model')
+Travis.logger.info('[hub] loading dependencies')
 require 'travis/model'
-
-Travis.logger.info('[hub] loading travis/states_cache')
 require 'travis/states_cache'
-
-Travis.logger.info('[hub] loading support/amqp')
 require 'travis/support/amqp'
-
-Travis.logger.info('[hub] loading hub/queue')
 require 'travis/hub/queue'
-
-Travis.logger.info('[hub] loading hub/error')
 require 'travis/hub/error'
-
-Travis.logger.info('[hub] loading hub/solo')
 require 'travis/hub/solo'
-
-Travis.logger.info('[hub] loading hub worker')
 require 'travis/hub/worker'
-
-Travis.logger.info('[hub] loading hub/dispatcher')
 require 'travis/hub/dispatcher'
-
-Travis.logger.info('[hub] loading hub/enqueue')
 require 'travis/hub/enqueue'
-
-Travis.logger.info('[hub] loading run_periodically')
 require 'core_ext/kernel/run_periodically'
+Travis.logger.info('[hub] done loading dependencies')
 
 $stdout.sync = true
 
