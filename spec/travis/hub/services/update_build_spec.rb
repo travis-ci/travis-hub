00b910d3c97578ec896b639812d523bd02a328a1
Sven Fuchs <me@svenfuchs.com>
2015-09-12 20:00:37 +0200

    add Services::UpdateBuild

--- spec/travis/hub/services/update_build_spec.rb
+++ spec/travis/hub/services/update_build_spec.rb
@@ -1,18 +1,36 @@
 describe Travis::Hub::Services::UpdateBuild do
-  let(:event)  { :cancel }
-  let(:state)  { :created }
   let(:build)  { FactoryGirl.create(:build, state: state, received_at: Time.now - 10) }
   let(:params) { { event: event, data: { id: build.id } } }
   subject      { described_class.new(params) }
 
-  it 'updates the build' do
-    subject.run
-    expect(build.reload.state).to eql(:canceled)
+  describe 'cancel event' do
+    let(:state) { :created }
+    let(:event) { :cancel }
+
+    it 'updates the build' do
+      subject.run
+      expect(build.reload.state).to eql(:canceled)
+    end
+
+    it 'instruments #run' do
+      subject.run
+      expect(stdout.string).to include("Travis::Hub::Services::UpdateBuild#run:completed event: cancel for <Build id=#{build.id}>")
+    end
   end
 
-  it 'instruments #run' do
-    subject.run
-    expect(stdout.string).to include("Travis::Hub::Services::UpdateBuild#run:completed event: cancel for <Build id=#{build.id}>")
+  describe 'restart event' do
+    let(:state) { :passed }
+    let(:event) { :restart }
+
+    it 'updates the build' do
+      subject.run
+      expect(build.reload.state).to eql(:created)
+    end
+
+    it 'instruments #run' do
+      subject.run
+      expect(stdout.string).to include("Travis::Hub::Services::UpdateBuild#run:completed event: restart for <Build id=#{build.id}>")
+    end
   end
 
   # describe 'with a canceled build' do
