934bbb884826e0af715228e5fb4f41a509902b0d
Sven Fuchs <me@svenfuchs.com>
2015-10-06 14:20:56 +0200

    rollout more traffic based on a percentage

--- lib/travis/hub/dispatcher.rb
+++ lib/travis/hub/dispatcher.rb
@@ -52,7 +52,23 @@ module Travis
       end
 
       def next?(job)
-        Travis::Features.enabled_for_all?(:hub_next) && NEXT.include?([job.owner_type, job.owner_id])
+        return false unless Travis::Features.enabled_for_all?(:hub_next)
+        next_const?(job) || next_rollout?(job)
+      end
+
+      def next_const?(job)
+        NEXT.include?([job.owner_type, job.owner_id])
+      end
+
+      def next_rollout?(job)
+        job.owner_id % 100 <= next_percent
+      end
+
+      def next_percent
+        percent = Travis.redis.get('hub_next_percent') || -1
+        percent.to_i
+      rescue
+        -1
       end
     end
   end
