fbf4c94134d9bacf10ce689ccd65df1257bd01ed
Sven Fuchs <me@svenfuchs.com>
2015-10-25 13:02:40 +0100

    add a rabbitmq to sidekiq drain

--- lib/travis/hub/app.rb
+++ lib/travis/hub/app.rb
@@ -1,5 +1,6 @@
 require 'travis/hub/app/context'
 require 'travis/hub/app/dispatcher'
+require 'travis/hub/app/drain'
 require 'travis/hub/app/handler'
 require 'travis/hub/app/solo'
 require 'travis/hub/app/worker'
@@ -7,14 +8,16 @@ require 'travis/hub/app/worker'
 module Travis
   module Hub
     class App
-      MODES = { solo: Solo, worker: Worker, dispatcher: Dispatcher }
+      MODES = { solo: Solo, worker: Worker, dispatcher: Dispatcher, drain: Drain }
 
       attr_reader :context, :processor
 
-      def initialize(mode, options)
-        Hub.context = @context = Context.new # TODO remove Hub.context
-        @processor = MODES.fetch(mode).new(context, mode, options)
-      end
+        def setup(context)
+          Database.connect(config.database.to_h)
+          Metrics.setup
+          setup_amqp   unless context == :sidekiq
+          setup_worker unless context == :dispatcher || context == :drain
+        end
 
       def run
         context.setup
---
+++ lib/travis/hub/app/drain.rb
@@ -0,0 +1,33 @@
+require 'sidekiq'
+require 'travis/hub/app/queue'
+
+module Travis
+  module Hub
+    module App
+      class Drain < Struct.new(:name, :options)
+        def run
+          Queue.subscribe(QUEUE, &method(:handle))
+        end
+
+        def handle(type, payload)
+          publish(type, payload)
+          meter(key)
+        end
+
+        private
+
+          def publish(type, payload)
+            ::Sidekiq::Client.push(
+              'queue'   => 'hub',
+              'class'   => 'Travis::Hub::Sidekiq::Worker',
+              'args'    => [type, payload]
+            )
+          end
+
+          def meter(key)
+            Metrics.meter("hub.#{name}.drain.#{key}")
+          end
+      end
+    end
+  end
+end
