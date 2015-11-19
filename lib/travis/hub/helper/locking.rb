02f7d12e4353df4daaf085bed7d52fb9cc4d555f
Sven Fuchs <me@svenfuchs.com>
2015-11-19 13:39:30 +0100

    try redis locks, bump travis-lock

--- lib/travis/hub/helper/locking.rb
+++ lib/travis/hub/helper/locking.rb
@@ -10,6 +10,13 @@ module Travis
 
           debug "Locking #{key} with: #{options[:strategy]}"
           Lock.exclusive(key, options, &block)
+        rescue Redis::TimeoutError => e
+          count ||= 0
+          raise e if count > 10
+          count += 1
+          error "Redis::TimeoutError while trying to acquire lock for #{key} (#{options}). Retrying #{count}/10."
+          sleep 1
+          retry
         end
       end
     end
