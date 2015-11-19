331f8c84acd750545bcc76a975ea98e42ef6c8c5
Sven Fuchs <me@svenfuchs.com>
2015-11-19 20:11:26 +0100

    log db config

--- lib/travis/hub/app/solo.rb
+++ lib/travis/hub/app/solo.rb
@@ -74,8 +74,8 @@ module Travis
           end
 
           def with_active_record(&block)
-            # ActiveRecord::Base.connection_pool.with_connection(&block)
-            yield
+            ActiveRecord::Base.connection_pool.with_connection(&block)
+            # yield
           rescue ActiveRecord::ConnectionTimeoutError => e
             count ||= 0
             raise e if count > 10
--- lib/travis/hub/support/database.rb
+++ lib/travis/hub/support/database.rb
@@ -2,11 +2,22 @@ module Travis
   module Hub
     module Database
       class << self
+        MSGS = {
+          setup: 'Setting up database connection with: %p'
+        }
+
         def connect(config, logger = nil)
           ActiveRecord::Base.establish_connection(config.to_h)
           ActiveRecord::Base.default_timezone = :utc
           ActiveRecord::Base.logger = logger
+          logger.info(MSGS[:setup] % except(config.to_h, :adapter, :username, :password)) if logger
         end
+
+        private
+
+          def except(hash, *keys)
+            hash.reject { |key, _| keys.include?(key) }
+          end
       end
     end
   end
