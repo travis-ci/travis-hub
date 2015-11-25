6724d9071ce099d119e1d3905f25602510ffa6f7
Sven Fuchs <me@svenfuchs.com>
2015-11-25 14:38:08 +0100

    reroute to next if enabled (for future rollouts)

--- lib/travis/hub/app/dispatcher.rb
+++ lib/travis/hub/app/dispatcher.rb
@@ -1,34 +1,41 @@
 require 'travis/hub/app/queue'
 require 'travis/hub/app/solo'
+require 'travis/hub/support/reroute'
 
 module Travis
   module Hub
     class App
       class Dispatcher < Solo
-        attr_accessor :count, :publishers
+        attr_reader :publishers
 
-        def initialize(context, name, options)
+        def initialize(*)
           super
-          @publishers = setup_publishers
+          @publishers = {}
         end
 
         private
 
           def handle(type, payload)
             with_active_record do
-              id  = ::Job.find(payload.fetch('id')).source_id
-              key = queue_for(id % count + 1)
-              # puts "Routing #{type} for <Job id=#{payload.fetch('id')}> to #{key}."
-              publishers[key].publish(payload.merge(worker_count: count), properties: { type: type })
-              meter("hub.#{name}.delegate.#{key}")
+              job = ::Job.find(payload.fetch('id'))
+              key = reroute?(job) ? :next : job.source_id % count + 1
+              puts "Routing #{type} for <Job id=#{job.id}> to #{queue_for(key)}."
+              publish(key, type, payload)
             end
           end
 
-          def setup_publishers
-            (1..count).inject({}) do |publishers, num|
-              name = queue_for(num)
-              publishers.merge(name => Travis::Amqp::Publisher.jobs(name))
-            end
+          def publish(key, type, payload)
+            publisher = publisher(queue_for(key))
+            publisher.publish(payload.merge(worker_count: count), properties: { type: type })
+            meter("hub.#{name}.delegate.#{key}")
+          end
+
+          def reroute?(job)
+            Reroute.new(:hub_next, id: job.id, owner_name: job.repository.owner_name).run
+          end
+
+          def publisher(name)
+            publishers[name] ||= Travis::Amqp::Publisher.jobs(name)
           end
 
           def queue_for(num)
