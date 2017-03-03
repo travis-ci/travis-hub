ecb51f0db8f70f10f91a23c50ed8850bbebbaee0
Sven Fuchs <me@svenfuchs.com>
2017-03-03 15:34:06 +0100

    change data format, add various durations

--- lib/travis/hub/serialize/keen/job.rb
+++ lib/travis/hub/serialize/keen/job.rb
@@ -1,32 +1,3 @@
-# This is what Gatekeeper sends:
-#
-#   {
-#     requests: [
-#       {
-#         event_type: 'push',
-#         matrix_size: 2,
-#         repository_id: 123,
-#         owner_id: 234,
-#         owner_type: 'User',
-#         owner: ['User', 234],
-#         language: 'default',
-#         language_version: {
-#           ruby: ['2.1.2', '2.2.2']
-#         github_language: nil,
-#         uses_sudo: false,
-#         uses_apt_get: false,
-#         dist_name: 'default',
-#         group_name: 'default'
-#       }
-#     ],
-#     deployments: [
-#       {
-#         provider: 's3',
-#         repository_id: 123
-#       }
-#     ],
-#     notifications: []
-#   }
 require 'date'
 
 module Travis
@@ -38,24 +9,32 @@ module Travis
             {
               jobs: [
                 {
-                  id:                  job.id,
-                  repository_id:       repo.id,
-                  repository_slug:     repo.slug,
-                  repository_private:  repo.private,
-                  owner_type:          job.owner_type,
-                  owner_id:            job.owner_id,
-                  owner_login:         job.owner.login,
-                  duration:            job.duration,
-                  number:              job.number,
-                  state:               job.state,
-                  queue:               job.queue,
-                  created_at:          job.created_at,
-                  received_at:         job.received_at,
-                  started_at:          job.started_at,
-                  queued_at:           job.queued_at,
-                  canceled_at:         job.canceled_at,
-                  finished_at:         job.finished_at,
-                  account_info:        account_info
+                  repository: {
+                    id:         repo.id,
+                    slug:       repo.slug,
+                    private:    repo.private,
+                  },
+                  owner: {
+                    type:       job.owner_type,
+                    id:         owner.id,
+                    login:      owner.login,
+                  },
+                  build: {
+                    id:         build.id,
+                    type:       request.event_type,
+                    number:     build.number,
+                    branch:     build.branch,
+                  },
+                  job: {
+                    id:         job.id,
+                    number:     job.number,
+                    state:      job.state,
+                    queue:      job.queue,
+                    wait_time:  wait_time,
+                    queue_time: queue_time,
+                    boot_time:  boot_time,
+                    run_time:   run_time,
+                  }
                 }
               ]
             }
@@ -63,29 +42,36 @@ module Travis
 
           private
 
-          # TODO
-          #
-          # I assume the intention of this is being able to query how many
-          # paid, educational, and trial builds we are running, right?
-          #
-          # We'd need to duplicate a lot of knowledge here in Hub just for
-          # publishing this information. E.g. a job could be authorized by
-          # delegating to another account in Scheduler. Trial builds are
-          # evaluated in Gatekeeper.
-          #
-          # Is Keen.io able to correlate information from several events? E.g.
-          # could we store trial information in Gatekeeper, job authorization
-          # info in Scheduler, job state information in Hub, and then have
-          # Keen.io correlate these bits into one?
-          #
-          # Or should this be stored in a column `account_info` on the builds
-          # or job table?
-            def account_info
-              # subscription, education user, educational org, trial account
-            end
-
             def repo
               job.repository
+            end
+
+            def owner
+              job.owner
+            end
+
+            def request
+              build.request
+            end
+
+            def build
+              job.build
+            end
+
+            def wait_time
+              job.queued_at - job.created_at
+            end
+
+            def queue_time
+              job.received_at - job.queued_at
+            end
+
+            def boot_time
+              job.started_at - job.received_at
+            end
+
+            def run_time
+              job.finished_at - job.started_at
             end
         end
       end
