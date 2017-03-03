e01185e5131ffa8cc5ae73da94ba64966eed79c1
Sven Fuchs <me@svenfuchs.com>
2017-03-03 15:34:06 +0100

    change data format, add various durations

--- spec/travis/hub/serialize/keen/job_spec.rb
+++ spec/travis/hub/serialize/keen/job_spec.rb
@@ -1,28 +1,34 @@
 describe Travis::Hub::Serialize::Keen::Job do
   let(:owner) { FactoryGirl.create(:user, login: 'login') }
-  let(:times) { %i(created_at received_at started_at queued_at canceled_at finished_at).map { |attr| [attr, Time.now] }.to_h }
-  let(:job)   { FactoryGirl.create(:job, times.merge(state: :passed, owner: owner)) }
+  let(:times) { %i(created_at queued_at received_at started_at finished_at).map.with_index { |attr, ix| [attr, Time.now + 60 * ix] }.to_h }
+  let(:job)   { FactoryGirl.create(:job, times.merge(state: :passed, owner: owner, queue: 'gce')) }
   let(:repo)  { job.repository }
+  let(:build) { job.build }
   let(:data)  { described_class.new(job).data[:jobs].first }
 
   before { Date.stubs(:today).returns(Date.parse('2016-10-14')) }
 
-  it { expect(data[:id]).to                  eq job.id }
-  it { expect(data[:repository_id]).to       eq repo.id }
-  it { expect(data[:repository_slug]).to     eq repo.slug }
-  it { expect(data[:repository_private]).to  eq repo.private }
-  it { expect(data[:owner_type]).to          eq 'User' }
-  it { expect(data[:owner_id]).to            eq owner.id }
-  it { expect(data[:owner_login]).to         eq 'login' }
-  it { expect(data[:created_at]).to          eq Time.now }
-  it { expect(data[:received_at]).to         eq Time.now }
-  it { expect(data[:started_at]).to          eq Time.now }
-  it { expect(data[:queued_at]).to           eq Time.now }
-  it { expect(data[:canceled_at]).to         eq Time.now }
-  it { expect(data[:finished_at]).to         eq Time.now }
-  it { expect(data[:duration]).to            eq job.duration }
-  it { expect(data[:number]).to              eq job.number }
-  it { expect(data[:state]).to               eq :passed }
-  it { expect(data[:queue]).to               eq job.queue }
-  it { expect(data[:account_info]).to        eq nil }
+  xit { expect(data[:account_type]).to        eq 'trial' }
+
+  it { expect(data[:repository][:id]).to      eq repo.id }
+  it { expect(data[:repository][:slug]).to    eq repo.slug }
+  it { expect(data[:repository][:private]).to eq false }
+
+  it { expect(data[:owner][:type]).to         eq 'User' }
+  it { expect(data[:owner][:id]).to           eq owner.id }
+  it { expect(data[:owner][:login]).to        eq 'login' }
+
+  it { expect(data[:build][:id]).to           eq job.build.id }
+  it { expect(data[:build][:type]).to         eq 'push' }
+  it { expect(data[:build][:number]).to       eq build.number }
+  it { expect(data[:build][:branch]).to       eq 'master' }
+
+  it { expect(data[:job][:id]).to             eq job.id }
+  it { expect(data[:job][:number]).to         eq job.number }
+  it { expect(data[:job][:state]).to          eq :passed }
+  it { expect(data[:job][:queue]).to          eq 'gce' }
+  it { expect(data[:job][:wait_time]).to      eq 60 }
+  it { expect(data[:job][:queue_time]).to     eq 60 }
+  it { expect(data[:job][:boot_time]).to      eq 60 }
+  it { expect(data[:job][:run_time]).to       eq 60 }
 end
