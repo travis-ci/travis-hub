aa46bbe6a097d884b98614e5d443dad2e660d123
Sven Fuchs <me@svenfuchs.com>
2017-03-13 15:37:10 +0100

    move publishing to Keen.io to the addons

---
+++ lib/travis/addons/handlers/keenio.rb
@@ -0,0 +1,48 @@
+require 'keen'
+require 'travis/addons/handlers/base'
+require 'travis/addons/serializer/keen/job'
+
+module Travis
+  module Addons
+    module Handlers
+      class Keenio < Base
+        EVENTS = 'job:finished'
+
+        MSGS = {
+          failed: 'Failed to push stats to keen.io: %s'
+        }
+
+        def handle?
+          ENV['KEEN_PROJECT_ID'] || ENV['ENV'] == 'test'
+        end
+
+        def handle
+          publish
+        end
+
+        private
+
+          def publish
+            ::Keen.publish_batch(data)
+          rescue ::Keen::HttpError => e
+            logger.error :failed, e.message
+          end
+
+          def data
+            @data ||= Serializer::Keen::Job.new(object).data
+          end
+
+          def logger
+            Addons.logger
+          end
+
+          class EventHandler < Addons::Instrument
+            def notify_completed
+              publish
+            end
+          end
+          EventHandler.attach_to(self)
+      end
+    end
+  end
+end
