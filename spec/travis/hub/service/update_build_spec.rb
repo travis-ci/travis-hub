be27424ee12ef9432ff35a9538353a1096cafe12
Sven Fuchs <me@svenfuchs.com>
2017-03-14 13:49:41 +0100

    meter auto-cancel per job

--- spec/travis/hub/service/update_build_spec.rb
+++ spec/travis/hub/service/update_build_spec.rb
@@ -1,11 +1,13 @@
 describe Travis::Hub::Service::UpdateBuild do
-  let(:now)   { Time.now }
-  let(:build) { FactoryGirl.create(:build, jobs: [job], state: state, received_at: now - 10) }
-  let(:job)   { FactoryGirl.create(:job, state: state) }
-  let(:amqp)  { Travis::Amqp.any_instance }
+  let(:now)     { Time.now }
+  let(:build)   { FactoryGirl.create(:build, jobs: [job], state: state, received_at: now - 10) }
+  let(:job)     { FactoryGirl.create(:job, state: state) }
+  let(:amqp)    { Travis::Amqp.any_instance }
+  let(:metrics) { Travis::Metrics }
 
   subject     { described_class.new(context, event, data) }
   before      { amqp.stubs(:fanout) }
+  before      { metrics.stubs(:meter) }
 
   describe 'start event' do
     let(:state) { :created }
@@ -102,7 +104,7 @@ describe Travis::Hub::Service::UpdateBuild do
   describe 'cancel event (gator, auto cancel)' do
     let(:state) { :created }
     let(:event) { :cancel }
-    let(:meta)  { { 'event' => 'pull_request', 'number' => '2', 'branch' => 'master', 'pull_request_number' => '1' } }
+    let(:meta)  { { 'auto' => true, 'event' => 'pull_request', 'number' => '2', 'branch' => 'master', 'pull_request_number' => '1' } }
     let(:data)  { { id: build.id, meta: meta } }
     let(:now) { Time.now }
 
@@ -125,6 +127,11 @@ describe Travis::Hub::Service::UpdateBuild do
     it 'adds an additional log line' do
       subject.run
       expect(job.log.parts.last.content).to include('This job was cancelled because the "Auto Cancellation" feature is currently enabled, and a more recent build (#2) for pull request #1 came in while this job was waiting to be processed.')
+    end
+
+    it 'meters the event' do
+      metrics.expects(:meter).with('hub.job.auto_cancel')
+      subject.run
     end
   end
 
