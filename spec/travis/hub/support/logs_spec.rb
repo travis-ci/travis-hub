6233602dff913dc6b9a407ee8780f013b9faee6c
Dan Buch <dan@travis-ci.org>
2017-03-14 20:42:56 -0400

    Fill in more specs for logs API adapter

--- spec/travis/hub/support/logs_spec.rb
+++ spec/travis/hub/support/logs_spec.rb
@@ -1,7 +1,33 @@
 describe Travis::Hub::Support::Logs do
   subject { described_class.new(config) }
 
-  let(:client) { mock('faraday') }
+  let :request_stubs do
+    Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.put '/logs/1' do |env|
+        [200, {}, '{}']
+      end
+
+      stub.put '/log-parts/2/last' do |env|
+        [200, {}, '{}']
+      end
+
+      stub.put '/logs/4' do |env|
+        [404, {}, '{}']
+      end
+
+      stub.put '/log-parts/3/last' do |env|
+        [404, {}, '{}']
+      end
+    end
+  end
+
+  let(:client) do
+    Faraday.new do |c|
+      c.response :raise_error
+      c.adapter :test, request_stubs
+    end
+  end
+
   let(:url) { 'http://loggo.example.com/' }
   let(:token) { 'fafafaf' }
   let(:config) { { url: url, token: token } }
@@ -11,24 +37,20 @@ describe Travis::Hub::Support::Logs do
   end
 
   it 'puts a body' do
-    client.expects(:put)
-    expect(subject.update(1, msg: 'flee flah floof')).to be_nil
+    expect(subject.update(1, msg: 'flee flah floof')).to be_success
   end
 
   it 'appends a log part' do
-    client.expects(:put)
-    expect(subject.append_log_part(2, 'furble flume')).to eq nil
+    expect(subject.append_log_part(2, 'furble flume')).to be_success
   end
 
   context 'when responses are not successful' do
     it 'does not put a body' do
-      client.expects(:put).raises(StandardError)
-      expect { subject.update(1, 'flaa flih fluuf') }
+      expect { subject.update(4, 'flaa flih fluuf') }
         .to raise_error(StandardError)
     end
 
     it 'does not append a log part' do
-      client.expects(:put).raises(StandardError)
       expect { subject.append_log_part(3, 'foibled fable') }
         .to raise_error(StandardError)
     end
