876aea38de9532aa83a0436b714d6e25167cfa03
Sven Fuchs <me@svenfuchs.com>
2017-03-29 14:22:53 +0200

    bring back stages tests

--- spec/travis/hub/model/stage_spec.rb
+++ spec/travis/hub/model/stage_spec.rb
@@ -13,63 +13,61 @@ describe Stage do
     jobs.map(&:reload)
   end
 
-  if ENV['BUILD_STAGES']
-    describe 'failure' do
-      before { jobs[0].finish!(state: :failed, finished_at: now) }
-      before { jobs[1].finish!(state: :passed, finished_at: now) }
-      before { reload }
+  describe 'failure' do
+    before { jobs[0].finish!(state: :failed, finished_at: now) }
+    before { jobs[1].finish!(state: :passed, finished_at: now) }
+    before { reload }
 
-      it { expect(build.state).to eq :failed }
-      it { expect(stages[0].state).to eq :failed }
-      it { expect(stages[1].state).to eq :canceled }
-      it { expect(stages[0].finished_at).to eq now }
-      it { expect(stages[1].finished_at).to eq now }
-      it { expect(jobs[0].state).to eq :failed }
-      it { expect(jobs[1].state).to eq :passed }
-      it { expect(jobs[2].state).to eq :canceled }
-    end
+    it { expect(build.state).to eq :failed }
+    it { expect(stages[0].state).to eq :failed }
+    it { expect(stages[1].state).to eq :canceled }
+    it { expect(stages[0].finished_at).to eq now }
+    it { expect(stages[1].finished_at).to eq now }
+    it { expect(jobs[0].state).to eq :failed }
+    it { expect(jobs[1].state).to eq :passed }
+    it { expect(jobs[2].state).to eq :canceled }
+  end
 
-    describe 'error' do
-      before { jobs[0].finish!(state: :errored, finished_at: now) }
-      before { jobs[1].finish!(state: :passed, finished_at: now) }
-      before { reload }
+  describe 'error' do
+    before { jobs[0].finish!(state: :errored, finished_at: now) }
+    before { jobs[1].finish!(state: :passed, finished_at: now) }
+    before { reload }
 
-      it { expect(build.state).to eq :errored }
-      it { expect(stages[0].state).to eq :errored }
-      it { expect(stages[1].state).to eq :canceled }
-      it { expect(jobs[0].state).to eq :errored }
-      it { expect(jobs[1].state).to eq :passed }
-      it { expect(jobs[2].state).to eq :canceled }
-    end
+    it { expect(build.state).to eq :errored }
+    it { expect(stages[0].state).to eq :errored }
+    it { expect(stages[1].state).to eq :canceled }
+    it { expect(jobs[0].state).to eq :errored }
+    it { expect(jobs[1].state).to eq :passed }
+    it { expect(jobs[2].state).to eq :canceled }
+  end
 
-    describe 'with allow_failure' do
-      before { jobs[0].update_attributes!(allow_failure: true) }
-      before { jobs[0].finish!(state: :failed, finished_at: now) }
-      before { jobs[1].finish!(state: :passed, finished_at: now) }
-      before { reload }
+  describe 'with allow_failure' do
+    before { jobs[0].update_attributes!(allow_failure: true) }
+    before { jobs[0].finish!(state: :failed, finished_at: now) }
+    before { jobs[1].finish!(state: :passed, finished_at: now) }
+    before { reload }
 
-      it { expect(build.state).to eq :started }
-      it { expect(stages[0].state).to eq :passed }
-      it { expect(stages[1].state).to eq :created }
-      it { expect(jobs[0].state).to eq :failed }
-      it { expect(jobs[1].state).to eq :passed }
-      it { expect(jobs[2].state).to eq :created }
-    end
+    it { expect(build.state).to eq :started }
+    it { expect(stages[0].state).to eq :passed }
+    it { expect(stages[1].state).to eq :created }
+    it { expect(jobs[0].state).to eq :failed }
+    it { expect(jobs[1].state).to eq :passed }
+    it { expect(jobs[2].state).to eq :created }
+  end
 
-    describe 'with allow_failure and fast_finish' do
-      before { build.update_attributes!(config: { matrix: { fast_finish: true } }) }
-      before { jobs[1].update_attributes!(allow_failure: true) }
-      before { jobs[0].finish!(state: :failed, finished_at: now) }
-      before { reload }
+  describe 'with allow_failure and fast_finish' do
+    before { build.update_attributes!(config: { matrix: { fast_finish: true } }) }
+    before { jobs[1].update_attributes!(allow_failure: true) }
+    before { jobs[0].finish!(state: :failed, finished_at: now) }
+    before { reload }
 
-      # i.e. the one `allow_failure` job will still be run (as it does with
-      # normal matrix builds) but jobs in later stages have been canceled
-      it { expect(build.state).to eq :failed }
-      it { expect(stages[0].state).to eq :failed }
-      it { expect(stages[1].state).to eq :canceled }
-      it { expect(jobs[0].state).to eq :failed }
-      it { expect(jobs[1].state).to eq :created }
-      it { expect(jobs[2].state).to eq :canceled }
-    end
+    # i.e. the one `allow_failure` job will still be run (as it does with
+    # normal matrix builds) but jobs in later stages have been canceled
+    it { expect(build.state).to eq :failed }
+    it { expect(stages[0].state).to eq :failed }
+    it { expect(stages[1].state).to eq :canceled }
+    it { expect(jobs[0].state).to eq :failed }
+    it { expect(jobs[1].state).to eq :created }
+    it { expect(jobs[2].state).to eq :canceled }
   end
 end
