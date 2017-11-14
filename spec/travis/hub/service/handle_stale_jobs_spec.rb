354fdef1dedcfd8ee96b8db45d58c33c9df8caf0
Piotr Sarnacki <drogus@gmail.com>
2017-11-14 13:15:32 +0100

    Fix handle stale jobs spec
    We introduced triggers to automatically set updated_at on jobs and
    builds. Because of that triggers will overwrite anything that's set by
    ActiveRecord and thus setting custom updated_at failed in the test. The
    solution is to disable triggers for the time of updating the updated_at
    column

--- spec/travis/hub/service/handle_stale_jobs_spec.rb
+++ spec/travis/hub/service/handle_stale_jobs_spec.rb
@@ -1,7 +1,16 @@
 describe Travis::Hub::Service::HandleStaleJobs do
   let(:now)         { Time.now }
-  let!(:job_old)    { FactoryGirl.create(:job, state: state, updated_at: now - 6 * 3600 - 10 ) }
-  let!(:job_new)    { FactoryGirl.create(:job, state: state, updated_at: now) }
+  let!(:job_old)    { FactoryGirl.create(:job, state: state) }
+  let!(:job_new)    { FactoryGirl.create(:job, state: state) }
+
+  before do
+    # we're using triggers to automatically set updated_at
+    # so if we want to force a give updated at we need to disable the trigger
+    ActiveRecord::Base.connection.execute("ALTER TABLE jobs DISABLE TRIGGER set_updated_at_on_jobs;")
+    job_old.update_attributes(updated_at: now - 6 * 3600 - 10)
+    job_new.update_attributes(updated_at: now)
+    ActiveRecord::Base.connection.execute("ALTER TABLE jobs ENABLE TRIGGER set_updated_at_on_jobs;")
+  end
 
   subject           { described_class.new(context) }
 
