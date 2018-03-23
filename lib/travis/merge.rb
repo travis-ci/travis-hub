aa45d31d64c6ba7278c67d05183ed58de3ec871c
Sven Fuchs <me@svenfuchs.com>
2018-03-23 17:44:39 +0100

    fetch merge api url from env

--- lib/travis/merge.rb
+++ lib/travis/merge.rb
@@ -4,8 +4,6 @@ module Travis
   module Merge
     extend self
 
-    URL = 'https://travis-merge-staging.herokuapp.com/api'
-
     def import(type, id, args)
       http.put([type, id].join('/'), args)
     end
@@ -13,13 +11,17 @@ module Travis
     private
 
       def http
-        Faraday.new(url: URL) do |c|
+        Faraday.new(url: url) do |c|
           c.request :authorization, :token, token
           c.request :retry, max: 5, interval: 0.05, interval_randomness: 0.5, backoff_factor: 2
           c.request :json
           c.response :raise_error
           c.adapter :net_http
         end
+      end
+
+      def url
+        ENV['MERGE_API_URL'] || 'https://travis-merge-staging.herokuapp.com/api'
       end
 
       def token
