1b152784362959b447cb363aa0974d51d9a19a31
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2018-04-18 15:22:33 +0200

    ignore installations that are no longer in place, see travis-pro/travis-hub#1

--- lib/travis/addons/handlers/github_status.rb
+++ lib/travis/addons/handlers/github_status.rb
@@ -11,7 +11,7 @@ module Travis
 
         def handle?
           if gh_apps_enabled?
-            installation = Installation.where(owner: repository.owner).first
+            installation = Installation.where(owner: repository.owner, removed_by: nil).first
             if installation
               payload.merge!({installation: installation.id})
               true
