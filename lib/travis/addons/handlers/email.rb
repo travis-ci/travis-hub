1bbd0357db6dced01e09f288ba1158eba60fbd58
Sergio Gil <sgilperez@gmail.com>
2018-08-30 11:23:07 +0200

    Skip build emails for users with no emails global preference

--- lib/travis/addons/handlers/email.rb
+++ lib/travis/addons/handlers/email.rb
@@ -31,6 +31,7 @@ module Travis
             emails = [commit.author_email, commit.committer_email]
             user_ids = object.repository.permissions.pluck(:user_id)
             user_ids -= object.repository.email_unsubscribes.pluck(:user_id)
+            user_ids -= User.where(id: user_ids).with_preference(:build_emails, false).pluck(:id)
             ::Email.where(email: emails, user_id: user_ids).pluck(:email).uniq
           end
 
