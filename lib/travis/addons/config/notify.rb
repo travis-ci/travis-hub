cedca37fda583caf25eb932a3360afab2fd48b92
Piotr Milcarz <piotrm@travis-ci.org>
2019-11-20 09:32:18 +0100

    freeze

--- lib/travis/addons/config/notify.rb
+++ lib/travis/addons/config/notify.rb
@@ -90,7 +90,7 @@ module Travis
           end
 
           def all_jobs_finished?
-            finish_states = %i[passed failed errored canceled]
+            finish_states = %i[passed failed errored canceled].freeze
             build.jobs.all? { |job| finish_states.include?(job.state) }
           end
 
