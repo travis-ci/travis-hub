ef62a9cf943a41ca2d0239f6a5d0b1dafe886613
Rafael Garcia <rafbgarcia@gmail.com>
2019-11-27 19:39:50 -0300

    Fix spec

--- spec/travis/addons/serializer/generic/build_spec.rb
+++ spec/travis/addons/serializer/generic/build_spec.rb
@@ -1,6 +1,6 @@
 describe Travis::Addons::Serializer::Tasks::Build do
   let(:owner)  { FactoryGirl.create(:user, login: 'login') }
-  let(:repo)   { FactoryGirl.create(:repository, github_id: 12345) }
+  let(:repo)   { FactoryGirl.create(:repository, github_id: 12345, vcs_id: '12345') }
   let(:build)  { FactoryGirl.create(:build, owner: owner, repository: repo, pull_request: pull, tag: tag, jobs: [job]) }
   let(:stage)  { FactoryGirl.create(:stage, number: 1, name: 'example') }
   let(:job)    { FactoryGirl.create(:job, repository: repo, stage: stage) }
@@ -40,6 +40,8 @@ describe Travis::Addons::Serializer::Tasks::Build do
     expect(data[:repository]).to eql(
       id: repo.id,
       github_id: 12345,
+      vcs_id: '12345',
+      vcs_type: 'GithubRepository',
       key: nil,
       name: 'travis-core',
       owner_name: 'travis-ci',
