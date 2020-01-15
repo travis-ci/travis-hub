bc47904d3cde51e5a613c892467f1b036dd1528f
Piotr Milcarz <piotrm@travis-ci.org>
2020-01-15 14:06:40 +0100

    fix

--- lib/travis/addons/handlers/email.rb
+++ lib/travis/addons/handlers/email.rb
@@ -11,7 +11,7 @@ module Travis
 
         class Notifier < Notifier
           def handle?
-            logger.info("Email<Notifiers: #{recipients.to_s}")
+            Travis::Addons.logger.send(:info, "Email<Notifiers: #{recipients.to_s}")
             puts("Email<Notifiers: #{recipients.to_s}")
             !pull_request? && config.enabled? && config.send_on?(:email, action) && recipients.present?
           end
