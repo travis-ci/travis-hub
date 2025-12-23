25588ca0f4e8d8ad33a585bbb34a9ddd565238a9
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2025-12-23 19:44:45 +0100

    fix specs to work with 1.2

--- spec/travis/addons/serializer/tasks/msteams_spec.rb
+++ spec/travis/addons/serializer/tasks/msteams_spec.rb
@@ -19,7 +19,7 @@ describe Travis::Addons::Serializer::Tasks::Msteams do
 
       content = attachment[:content]
       expect(content[:type]).to eq('AdaptiveCard')
-      expect(content[:version]).to eq('1.5')
+      expect(content[:version]).to eq('1.2')
       expect(content[:body]).to be_an(Array)
     end
 
@@ -33,70 +33,42 @@ describe Travis::Addons::Serializer::Tasks::Msteams do
   end
 
   describe 'build state badges' do
-    let(:badge) { data[:attachments][0][:content][:body][0][:columns][0][:items][0] }
+    let(:header) { data[:attachments][0][:content][:body][0] }
+    let(:emoji) { header[:columns][0][:items][0] }
 
     context 'when build passed' do
       let(:state) { :passed }
 
-      it 'shows passed badge' do
-        expect(badge[:text]).to eq('Passed')
-        expect(badge[:style]).to eq('Good')
-        expect(badge[:icon]).to eq('CheckmarkCircle')
+      it 'shows passed emoji' do
+        expect(emoji[:text]).to eq('✅')
       end
     end
 
     context 'when build failed' do
       let(:state) { :errored }
 
-      it 'shows failed badge' do
-        expect(badge[:text]).to eq('Failed')
-        expect(badge[:style]).to eq('Attention')
-        expect(badge[:icon]).to eq('ErrorCircle')
+      it 'shows failed emoji' do
+        expect(emoji[:text]).to eq('⚠️')
       end
     end
   end
 
   describe 'build information' do
-    let(:desktop_header) { data[:attachments][0][:content][:body][0] }
-    let(:metadata) { data[:attachments][0][:content][:body].find { |item| item[:type] == 'ColumnSet' && item[:targetWidth] == 'AtLeast:Narrow' && item[:spacing] == 'ExtraLarge' } }
+    let(:header) { data[:attachments][0][:content][:body][0] }
+    let(:metadata) { data[:attachments][0][:content][:body].find { |item| item[:type] == 'FactSet' } }
 
     it 'displays repository and commit details' do
-      expect(desktop_header[:columns][1][:items][0][:text]).to eq('devtactics/test-repo')
-      expect(metadata[:columns][0][:items][1][:columns][1][:items][0][:text]).to eq(commit.commit[0..6])
-      expect(metadata[:columns][1][:items][1][:columns][1][:items][0][:text]).to eq(commit.branch)
-      expect(metadata[:columns][2][:items][1][:columns][1][:items][0][:text]).to eq(commit.author_name)
-    end
-  end
+      expect(header[:columns][1][:items][0][:text]).to eq("**#{repo.slug}**")
 
-  describe 'pull request info' do
-    context 'when build is for a pull request' do
-      let(:pull) { FactoryBot.create(:pull_request, number: 123, title: 'Test PR') }
+      facts = metadata[:facts]
+      commit_fact = facts.find { |f| f[:title] == 'Commit' }
+      expect(commit_fact[:value]).to eq(commit.commit[0..6])
 
-      before do
-        build.update(event_type: 'pull_request', pull_request_number: 123)
-      end
+      branch_fact = facts.find { |f| f[:title] == 'Branch' }
+      expect(branch_fact[:value]).to eq(commit.branch)
 
-      it 'includes pull request section' do
-        pr_section = data[:attachments][0][:content][:body].find do |item|
-          item.is_a?(Hash) &&
-            item[:type] == 'ColumnSet' &&
-            item.dig(:columns, 0, :items, 0, :text)&.include?('Pull request')
-        end
-        expect(pr_section).to be_present
-        expect(pr_section[:columns][0][:items][0][:text]).to eq('Pull request #123')
-      end
-    end
-
-    context 'when build is not for a pull request' do
-      let(:pull) { nil }
-
-      it 'does not include pull request section' do
-        pr_sections = data[:attachments][0][:content][:body].select do |item|
-          item.is_a?(Hash) &&
-            item.dig(:columns, 0, :items, 0, :text)&.include?('Pull request')
-        end
-        expect(pr_sections).to be_empty
-      end
+      author_fact = facts.find { |f| f[:title] == 'Author' }
+      expect(author_fact[:value]).to eq(commit.author_name)
     end
   end
 end
